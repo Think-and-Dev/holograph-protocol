a62e4026e466fcba385f3bf1b973e22d3b2cc365
Léo FASANO <fasanol.info@gmail.com>
2024-04-09 19:22:23 +0200

    chore: removed debug and useless jobs

--- scripts/check_rpc.sh
+++
@@ -1,9 +0,0 @@
-#bin
-
-echo "Checking RPC connection to anvil :8545..."
-curl -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"net_version","params":[],"id":1}' http://localhost:8545
-
-echo ""
-echo ""
-echo "Checking RPC connection to anvil :9545..."
-curl -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"net_version","params":[],"id":1}' http://localhost:9545
