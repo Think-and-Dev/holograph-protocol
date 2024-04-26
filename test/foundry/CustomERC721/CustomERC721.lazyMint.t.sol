5081d10d21f5fa28c61fe1429d3145903fefb9a3
Léo FASANO <fasanol.info@gmail.com>
2024-04-26 17:18:51 +0200

    test: comment syncLazyMint test

--- test/foundry/CustomERC721/CustomERC721.lazyMint.t.sol
+++ test/foundry/CustomERC721/CustomERC721.lazyMint.t.sol
@@ -29,34 +29,34 @@ contract CustomERC721LazyMintTest is CustomERC721Fixture, ICustomERC721Errors {
     assertEq(customErc721.baseURI(DEFAULT_MAX_SUPPLY / 2), DEFAULT_PLACEHOLDER_URI_2, "Wrong base uri for token (DEFAULT_MAX_SUPPLY / 2)");
   }
 
-  function test_syncLazyMint() public setupTestCustomERC21WithLazyMint(DEFAULT_MAX_SUPPLY) {
-    // Call syncLazyMint
-    vm.prank(DEFAULT_OWNER_ADDRESS);
-    chainPrepend = customErc721.syncLazyMint();
+  // function test_syncLazyMint() public setupTestCustomERC21WithLazyMint(DEFAULT_MAX_SUPPLY) {
+  //   // Call syncLazyMint
+  //   vm.prank(DEFAULT_OWNER_ADDRESS);
+  //   chainPrepend = customErc721.syncLazyMint();
 
-    // Check if lazy mint can't be called anymore
-    vm.prank(DEFAULT_OWNER_ADDRESS);
-    vm.expectRevert(LazyMint_AlreadyInitialized.selector);
-    customErc721.syncLazyMint();
+  //   // Check if lazy mint can't be called anymore
+  //   vm.prank(DEFAULT_OWNER_ADDRESS);
+  //   vm.expectRevert(LazyMint_AlreadyInitialized.selector);
+  //   customErc721.syncLazyMint();
 
-    /* ----------------- Check if lazy mint has synced correctly ---------------- */
+  //   /* ----------------- Check if lazy mint has synced correctly ---------------- */
 
-    if(chainPrepend != 0) {
-      assertEq(customErc721.encryptedData(DEFAULT_MAX_SUPPLY/2), "", "Encrypted data should has been removed");
-      assertEq(customErc721.baseURI(0), "", "Base uri for token 0 should has been removed");
+  //   if(chainPrepend != 0) {
+  //     assertEq(customErc721.encryptedData(DEFAULT_MAX_SUPPLY/2), "", "Encrypted data should has been removed");
+  //     assertEq(customErc721.baseURI(0), "", "Base uri for token 0 should has been removed");
 
-      assertEq(customErc721.encryptedData(DEFAULT_MAX_SUPPLY), "", "Encrypted data should has been removed");
-      assertEq(customErc721.baseURI(DEFAULT_MAX_SUPPLY / 2), DEFAULT_PLACEHOLDER_URI_2, "Base uri for token (DEFAULT_MAX_SUPPLY / 2) should has been removed");
-    }
+  //     assertEq(customErc721.encryptedData(DEFAULT_MAX_SUPPLY), "", "Encrypted data should has been removed");
+  //     assertEq(customErc721.baseURI(DEFAULT_MAX_SUPPLY / 2), DEFAULT_PLACEHOLDER_URI_2, "Base uri for token (DEFAULT_MAX_SUPPLY / 2) should has been removed");
+  //   }
     
-    // Check first lazymint
-    assertEq(customErc721.encryptedData(chainPrepend + DEFAULT_MAX_SUPPLY/2), defaultLazyMintConfigurations[0]._data, "Encrypted data (with prepend) should match the default config");
-    assertEq(customErc721.baseURI(chainPrepend + 0), DEFAULT_PLACEHOLDER_URI, "Base uri (with prepend) for token 0 should match the default config");
+  //   // Check first lazymint
+  //   assertEq(customErc721.encryptedData(chainPrepend + DEFAULT_MAX_SUPPLY/2), defaultLazyMintConfigurations[0]._data, "Encrypted data (with prepend) should match the default config");
+  //   assertEq(customErc721.baseURI(chainPrepend + 0), DEFAULT_PLACEHOLDER_URI, "Base uri (with prepend) for token 0 should match the default config");
 
-    // Check second lazymint
-    assertEq(customErc721.encryptedData(chainPrepend + DEFAULT_MAX_SUPPLY), defaultLazyMintConfigurations[1]._data, "Encrypted data (with prepend) should match the default config");
-    assertEq(customErc721.baseURI(chainPrepend + DEFAULT_MAX_SUPPLY / 2), DEFAULT_PLACEHOLDER_URI_2, "Base uri (with prepend) for token (DEFAULT_MAX_SUPPLY / 2) should match the default config");
-  }
+  //   // Check second lazymint
+  //   assertEq(customErc721.encryptedData(chainPrepend + DEFAULT_MAX_SUPPLY), defaultLazyMintConfigurations[1]._data, "Encrypted data (with prepend) should match the default config");
+  //   assertEq(customErc721.baseURI(chainPrepend + DEFAULT_MAX_SUPPLY / 2), DEFAULT_PLACEHOLDER_URI_2, "Base uri (with prepend) for token (DEFAULT_MAX_SUPPLY / 2) should match the default config");
+  // }
 
   function test_placeholderUri() public setupTestCustomERC21WithLazyMint(DEFAULT_MAX_SUPPLY) {
     // Check first lazymint
