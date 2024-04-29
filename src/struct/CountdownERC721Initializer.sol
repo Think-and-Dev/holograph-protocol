0ad0e107be17b7753b4296900f5349970ea54dbe
Alexander <alexanderattar@gmail.com>
2024-04-29 17:41:30 -0400

    Better comments for CountdownERC721Initializer

--- src/struct/CountdownERC721Initializer.sol
+++ src/struct/CountdownERC721Initializer.sol
@@ -4,20 +4,23 @@ pragma solidity 0.8.13;
 
 import {CustomERC721SalesConfiguration} from "src/struct/CustomERC721SalesConfiguration.sol";
 
-/// @param initialOwner User that owns and can mint the edition, gets royalty and sales payouts and can update the base url if needed.
-/// @param initialMinter User that can mint on behalf of those who purchase offchain
-/// @param fundsRecipient Wallet/user that receives funds from sale
-/// @param mintTimeCost The time to subtract from the countdownEnd on each mint
-/// @param countdownEnd The countdown end time
-/// @param royaltyBPS BPS of the royalty set on the contract. Can be 0 for no royalty.
-/// @param salesConfiguration The initial SalesConfiguration
+/// @title A struct for initializing a CountdownERC721 contract
+/// @dev This struct is used during the deployment of a CountdownERC721 to set initial configuration parameters.
+/// @param startDate The maximum start date
+/// @param initialMaxSupply The maximum initial supply.
+/// @param mintInterval The maximum interval between mints
+/// @param initialOwner The user that owns the contract, can mint tokens, receives royalty and sales payouts, and can update the base URL if needed.
+/// @param initialMinter The user that is allowed to mint tokens on behalf of others, typically for offchain purchasers.
+/// @param fundsRecipient The wallet or user that receives funds from token sales.
+/// @param contractURI The URI for the contract metadata.
+/// @param salesConfiguration The initial sales configuration settings, defining how tokens are sold.
 struct CountdownERC721Initializer {
-  uint40 startDate; // max start date in year 36_812
-  uint32 initialMaxSupply; // max initial supply 4_294_967_295 tokens
-  uint24 mintInterval; // max mint interval 16_777_215 seconds
-  address initialOwner;
-  address initialMinter;
-  address payable fundsRecipient;
-  string contractURI;
-  CustomERC721SalesConfiguration salesConfiguration;
+  uint40 startDate; // The starting date for the countdown
+  uint32 initialMaxSupply; // The theoretical initial maximum supply of tokens at the start of the countdown.
+  uint24 mintInterval; // The interval between possible mints,
+  address initialOwner; // Address of the initial owner, who has administrative privileges.
+  address initialMinter; // Address of the initial minter, who can mint new tokens for those who purchase offchain.
+  address payable fundsRecipient; // Address of the recipient for funds gathered from sales.
+  string contractURI; // URI for the metadata associated with this contract.
+  CustomERC721SalesConfiguration salesConfiguration; // Configuration of sales settings for this contract.
 }
