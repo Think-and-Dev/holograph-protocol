296b0561eca18da74035a195c40e29804ecfb62a
Mercedes <mercedes@thinkanddev.com>
2024-06-18 10:16:49 -0300

    Update Constants.sol

--- test/foundry/utils/Constants.sol
+++ test/foundry/utils/Constants.sol
@@ -143,15 +143,19 @@ library Constants {
   function getDropsEventConfig() internal pure returns (uint256) {
     return 0x0000000000000000000000000000000000000000000000000000000000040000;
   }
+
   function getHolographDropERC721V2() internal pure returns (address) {
     return address(0x30F9D1c28584e0874dEE1b2f0101D77077D316e4);
   }
+
   function getEditionsMetadataRendererProxy() internal pure returns (address) {
     return address(0xdF26982B2D5A4904757f6099b939c0eBcFE70668);
   }
+
   function getHolographDropERC721() internal pure returns (address) {
     return address(0xc4aE0619B36BC57227DC258472E57A7265C5f2aA);
   }
+
   function getHolographIdL1() internal pure returns (uint32) {
     return 4294967294;
   }
@@ -171,6 +175,7 @@ library Constants {
   address public constant zeroAddress = address(0x0000000000000000000000000000000000000000);
   address public constant originAddress = address(0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38);
   bytes32 public constant saltHex = bytes32(0x00000000000000000000000000000000000000000000000000000000000003e8);
+  bytes32 public constant eventConfig = bytes32(0x0000000000000000000000000000000000000000000000000000000000000086);
 }
 
 library ErrorConstants {
